b863ec1bd00be7f7c2674e21bf83c5b1769c6150
julienr2 <julien.rougeron@nouma.fr>
2017-10-12 02:20:25 +0200

    add formulaire

--- src/components/BookmarkAdd.vue
+++ src/components/BookmarkAdd.vue
@@ -1,43 +1,44 @@
 <template>
-
-      <div class="card" v-if="display">
-        <div class="row">
-          <div class="col-sm-10">
-              <input v-model="newBookmark.url" debounce="500" class="form-control mr-sm-6" type="text" placeholder="Enter bookmark url" >
-          </div>
-          <div class="col-sm-2">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit" @click="addNewBookmark" >Add</button>
-          </div>
-        </div>
+<form novalidate @submit.stop.prevent="submit" autocomplete="on">
+  <div class="card" v-if="display">
+    <div class="row">
+      <div class="col-sm-10">
+        <input v-model="newBookmark.url" debounce="500" class="form-control mr-sm-6" type="text" placeholder="Enter bookmark url">
       </div>
-
-
+      <div class="col-sm-2">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Add</button>
+      </div>
+    </div>
+  </div>
+</form>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        display: true,
-        newBookmark: {
-          'url': '',
-          'title': '',
-          'description': '',
-          'source': ''
-        }
-      }
-    },
-    methods: {
-      addNewBookmark() {
-        console.log(this.newBookmark)
-        this.$store.dispatch('addBookmark', { bookmark: this.newBookmark })
+export default {
+  data() {
+    return {
+      display: true,
+      newBookmark: {
+        'url': '',
+        'title': '',
+        'description': '',
+        'source': ''
       }
     }
+  },
+  methods: {
+    submit() {
+      console.log(this.newBookmark)
+      this.$store.dispatch('addBookmark', {
+        bookmark: this.newBookmark
+      })
+    }
   }
+}
 </script>
 
-<style scoped> 
-.card{
+<style scoped>
+.card {
   margin-bottom: 15px;
   padding: 15px;
 }
