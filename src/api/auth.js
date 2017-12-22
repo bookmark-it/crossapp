7b3a1c5976c7d8a232fbb5e64ba7ae603bdb9abe
julienr2 <julien.rougeron@nouma.fr>
2017-12-23 00:51:29 +0100

    update api endpoint after djoser udpate

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 export default {
   login ({username, password}) {
-    return Vue.http.post('auth/login/', {username, password})
+    return Vue.http.post('auth/token/create/', {username, password})
   },
   fetchUserInfo () {
     return Vue.http.get('me')
